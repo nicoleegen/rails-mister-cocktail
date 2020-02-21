3a6960659b404a1fe7c05ed8c6eeae1d96f532db
nicole egen <nicole.egen@web.de>
2020-02-21 15:16:30 +0100

    green rake rails mister cocktail

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -4,12 +4,31 @@ class DosesController < ApplicationController
     @dose = Dose.new
   end
 
-  def create
+  def show
+    @dose = Dose.find(params[:id])
   end
 
-  def delete
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    # raise
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    cocktail = @dose.cocktail
+    @dose.destroy
+    redirect_to cocktail_path(cocktail)
   end
 
   private
-
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
 end
